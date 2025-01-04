6ae5ed51143450447b8db55266dd376158f06ba6
ramdesh <rasade88@gmail.com>
2025-01-04 01:37:57 -0600

    :truck: Moved module order

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
+module "my-sns" {
+  source                      = "./sns"
+  cost_alert_email_recipients = var.cost_alert_email_recipients
+}
+
 module "my-budget" {
   source                    = "./budgets"
   daily_cost_limit          = var.daily_cost_limit
   monthly_cost_limit        = var.monthly_cost_limit
   subscriber_sns_topic_arns = module.my-sns.budget_subscriber_topic_arns
-}
-
-module "my-sns" {
-  source                      = "./sns"
-  cost_alert_email_recipients = var.cost_alert_email_recipients
 }
